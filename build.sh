d42c3f9029c09ad3cb001386e503a712701054b7
Subin Sebastian <subin@xtel.in>
2015-04-23 09:33:19 +0530

    Blog updates

---
+++ build.sh
@@ -0,0 +1,9 @@
+jekyll build --destination output
+cp output/* ~/Public/subinsebastien.github.com
+cd ~/Public/subinsebastien.github.com/
+git rm -r --cached .
+git add .
+git commit -m "Blog updates"
+git push -u origin master
+cd ~/Downloads/Personal\ Blog/blog/
+rm -r output
